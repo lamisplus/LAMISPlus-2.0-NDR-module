d19250db7c85188f7d8524edc8673a6db371b368
Mathew Adegbite <mathewadegbite@gmail.com>
2022-07-27 06:46:21 +0100

    Finally testing the build

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
 //export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjU4ODQ5NTIwfQ.4Qs_ihDvgopZfNiXxYaHfNVUnxV1TiMGZgqaduphg2APTsO3QnTM_-sDpx_flBrA3pA-pSvUoynyzY-D12uKrQ';
 export const  token = (new URLSearchParams(window.location.search)).get("jwt")
-export const url = '/api/'
+export const url = '/api/v1/'
 //export const url =  'http://localhost:8282/api/v1/';
